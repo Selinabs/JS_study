7cf3d49d66c2c9922e489c2bb2e9f01327982e1d
梓轩丶 <20925149@zju.edu.cn>
2020-06-26 22:59:41 +0800

    222

---
+++ 4-4.js
@@ -0,0 +1,14 @@
+var level = prompt('请输入A，B，C，D')
+var str
+switch (level) {
+    case "A":
+        srt = "你的成绩优秀"
+        
+        break;
+
+    default:
+        str = "你输入的成绩"
+        break;
+}
+
+console.log(str)
