377bc41693dd1ed958da0ce07eac99113345bedc
梓轩丶 <20925149@zju.edu.cn>
2020-08-13 23:04:08 +0800

    python选择排序

--- js.py
+++ js.py
@@ -5,11 +5,31 @@
     
 
 # print(a)
-a = 0
-b = 0
-for i in range(1,101):
-    if i % 7 == 0:
-        print(i)
-        a += 1
-        b += i
-print(a,b)
+# a = 0
+# b = 0
+# for i in range(1,101):
+#     if i % 7 == 0:
+#         print(i)
+#         a += 1
+#         b += i
+# print(a,b)
+# 
+#
+#     选择排序
+
+a = [4,76,45,3,6,34,23,89]
+
+def about_min(t):
+    print(t)
+    for i in range(len(t)):
+        minIndex = i
+        for j in range(len(t)):
+            if t[j] > t[minIndex]:
+                minIndex = j
+            b = t[minIndex]
+            t[minIndex] = t[i]
+            t[i] = b
+    return t
+
+x = about_min(a)
+print(x)
